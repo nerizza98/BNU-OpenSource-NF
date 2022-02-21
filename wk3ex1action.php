d5bfd2b214a9434e1a996c33d3a0dea5d4499caa
nerizza98 <71983728+nerizza98@users.noreply.github.com>
2022-02-21 15:25:04 +0000

    Create wk3ex1action.php

---
+++ wk3ex1action.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Response to form</title>
+</head>
+<body>
+<?php
+	echo "Your name is $_POST[txtname]";
+	echo "Your gender is $_POST[radsex]";
+	echo "Your occupation is $_POST[seloccupation]";
+?>
+</body>
+</html>
